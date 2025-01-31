606a87ba68b29797b9e967e01f49d3e020923ee7
Maximillionair <maxmillian.heim@outlook.com>
2025-01-31 15:13:32 +0100

    nettstedet lastet i evigheter om jeg brukte cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
+// const cors = require('cors');
 const router = require('./router/routes.js'); 
 const app = express();
 const {connectDB} = require("./handlers/dbhandler.js");
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+// app.use(cors);
 app.use(router);
 
 const PORT = process.env.PORT || 3150;
